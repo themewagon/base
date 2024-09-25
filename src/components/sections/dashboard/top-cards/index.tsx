e8600d7f95a6a99eb2a5680f3a94636fa0f7c0e6
Riazul Islam <riazul5207@gmail.com>
2024-09-25 11:14:11 +0600

    dashboard page layout created

--- src/components/sections/dashboard/top-cards/index.tsx
+++ src/components/sections/dashboard/top-cards/index.tsx
@@ -1,31 +1,33 @@
-import { Box, Paper, Stack, Typography } from '@mui/material';
+import { Box, Paper, Stack, Typography, Grid } from '@mui/material';
 import IconifyIcon from 'components/base/IconifyIcon';
 
 const TopCards = () => {
   return (
-    <Stack spacing={2} justifyContent="space-between">
-      <Stack component={Paper} p={2.5} alignItems="center" spacing={2.25} height={100}>
-        <Stack
-          alignItems="center"
-          justifyContent="center"
-          height={56}
-          width={56}
-          bgcolor="info.main"
-          borderRadius="50%"
-        >
-          <IconifyIcon icon="ic:round-bar-chart" fontSize="h2.fontSize" color="primary.main" />
-        </Stack>
+    <Grid container spacing={2.5}>
+      <Grid item xs={12} sm={6} md={4} xl={2}>
+        <Stack component={Paper} p={2.5} alignItems="center" spacing={2.25} height={100}>
+          <Stack
+            alignItems="center"
+            justifyContent="center"
+            height={56}
+            width={56}
+            bgcolor="info.main"
+            borderRadius="50%"
+          >
+            <IconifyIcon icon="ic:round-bar-chart" fontSize="h2.fontSize" color="primary.main" />
+          </Stack>
 
-        <Box>
-          <Typography variant="h5" color="text.secondary">
-            178+
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Save Products
-          </Typography>
-        </Box>
-      </Stack>
-    </Stack>
+          <Box>
+            <Typography variant="h5" color="text.secondary">
+              178+
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Save Products
+            </Typography>
+          </Box>
+        </Stack>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,13 @@
-import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import TopCards from 'components/sections/dashboard/top-cards';
 
 const Dashboard = () => {
   return (
-    <Box>
-      <TopCards />
-    </Box>
+    <Grid container px={3} spacing={2.5}>
+      <Grid item xs={12}>
+        <TopCards />
+      </Grid>
+    </Grid>
   );
 };
 
