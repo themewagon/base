e72c898469c0f8d4b508cc4285b2b7945a81b0ee
riazul islam <riazul.codes@gmail.com>
2026-02-27 08:44:39 +0600

    feat: add reports section, customized echart

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,15 @@
 import Grid from '@mui/material/Grid';
 import KPIs from 'components/sections/dashboard/kpis/KPIs';
+import Reports from 'components/sections/dashboard/reports/Reports';
 
 const Dashboard = () => {
   return (
     <Grid container spacing={3.75} px={3.75}>
       <Grid size={{ xs: 12 }}>
         <KPIs />
+      </Grid>
+      <Grid size={{ xs: 12, md: 7 }}>
+        <Reports />
       </Grid>
     </Grid>
   );
