65258f6581254d14c5456a94a4f29de11056d2b3
riazul islam <riazul.codes@gmail.com>
2026-02-28 15:14:39 +0600

    feat: add analytics, recent-orders and top-selling sections

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,9 @@
 import Grid from '@mui/material/Grid';
 import KPIs from 'components/sections/dashboard/kpis/KPIs';
 import Reports from 'components/sections/dashboard/reports/Reports';
+import Analytics from 'components/sections/dashboard/analytics/Analytics';
+import RecentOrders from 'components/sections/dashboard/recent-orders/RecentOrders';
+import TopSelling from 'components/sections/dashboard/top-selling/TopSelling';
 
 const Dashboard = () => {
   return (
@@ -10,6 +13,15 @@ const Dashboard = () => {
       </Grid>
       <Grid size={{ xs: 12, md: 7 }}>
         <Reports />
+      </Grid>
+      <Grid size={{ xs: 12, md: 5 }}>
+        <Analytics />
+      </Grid>
+      <Grid size={{ xs: 12, md: 7 }}>
+        <RecentOrders />
+      </Grid>
+      <Grid size={{ xs: 12, md: 5 }}>
+        <TopSelling />
       </Grid>
     </Grid>
   );
